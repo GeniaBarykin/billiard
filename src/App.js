a0381c274833203324607d0793f83c44832a622e
GeniaBarykin <467232@student.saxion.nl>
2024-03-28 22:20:11 +0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Canvas from "./Canvas/Canvas"
 
 function App() {
-  const text = "Hello world 2";
   return (
     <div className="App">
       Водите нажатой мышкой вне шара, чтобы толкать.
