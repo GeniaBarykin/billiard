1d1491ba75f0bbc547b11f7452ace92246761cb6
GeniaBarykin <467232@student.saxion.nl>
2024-03-28 22:03:55 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const text = "Hello world 2";
   return (
     <div className="App">
+      Нажмите мышкой вне шара, чтобы толкать.
       <Canvas id="canvas" />
     </div>
   );
