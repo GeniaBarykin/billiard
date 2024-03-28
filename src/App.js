45d7c9b8b70db256e5b73be005ac3b3d7e1452e0
GeniaBarykin <467232@student.saxion.nl>
2024-03-28 19:09:46 +0300

    put into react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Canvas from "./Canvas/Canvas"
+
+function App() {
+  const text = "Hello world 2";
+  return (
+    <div className="App">
+      <Canvas id="canvas" />
+    </div>
+  );
+}
+
+export default App;
